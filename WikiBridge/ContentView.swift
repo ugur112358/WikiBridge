0b5f1b04f30477f091395b0d119b272c7e2ac4c9
val <vally10@live.com>
2026-05-29 19:35:56 +0200

    Initial Commit

---
+++ WikiBridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WikiBridge
+//
+//  Created by Ugur on 29/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WikiBridge/WikiBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiBridgeApp.swift
+//  WikiBridge
+//
+//  Created by Ugur on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WikiBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
